b49dfbc4b93e77625735047cf8dc91cb5dc7d74b
Vadim <opizah70@gmail.com>
2022-12-15 22:38:02 +0500

    dz

---
+++ dzz/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
